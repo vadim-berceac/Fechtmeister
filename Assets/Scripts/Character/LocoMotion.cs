8b72e999dc4cc0bfd6e92d78b43aebc3178f8673
v.berchak <v.berchak@nekki.com>
2025-04-28 20:53:11 +0300

    +

--- Assets/Scripts/Character/LocoMotion.cs
+++ Assets/Scripts/Character/LocoMotion.cs
@@ -38,6 +38,7 @@ public class LocoMotion : IInputHandler
             return;
         }
         _characterInputSet.OnMove += OnMove;
+        _characterInputSet.OnMove += RotateCharacter;
     }
 
     private void Unsubscribe()
@@ -47,12 +48,23 @@ public class LocoMotion : IInputHandler
             return;
         }
         _characterInputSet.OnMove -= OnMove;
+        _characterInputSet.OnMove -= RotateCharacter;
     }
 
     private void OnMove(Vector2 move)
     {
         _locoMotionSettings.Animator.SetFloat(_locoMotionSettings.InputX, move.x);
         _locoMotionSettings.Animator.SetFloat(_locoMotionSettings.InputY, move.y);
+    }
+    
+    private void RotateCharacter(Vector2 input)
+    {
+        if (input.sqrMagnitude < 0.01f) return;
+        Vector3 inputDirection = new Vector3(input.x, 0, input.y).normalized;
+        Quaternion targetRotation = Quaternion.LookRotation(inputDirection);
+        Quaternion newRotation = Quaternion.Slerp(_locoMotionSettings.Rigidbody.rotation, targetRotation, _locoMotionSettings.RotationSpeed * Time.fixedDeltaTime);
+        _locoMotionSettings.Rigidbody.MoveRotation(newRotation);
+        Debug.Log($"Applied Rotation: {_locoMotionSettings.Rigidbody.rotation}");
     }
 }
 
@@ -61,7 +73,13 @@ public struct LocoMotionSettings
 {
     [field: Header("Animator")]
     [field: SerializeField] public Animator Animator { get; private set; }
+    
+    [field: Header("Rigidbody")]
+    [field: SerializeField] public Rigidbody Rigidbody { get; private set; }
+    
     [field: Header("Animation Params")]
+    [field: SerializeField] public float RotationSpeed { get; private set; }
+    [field: SerializeField] public float MaxSlopeAngle { get; private set; }
     [field: SerializeField] public string InputY { get; private set; }
     [field: SerializeField] public string InputX { get; private set; }
 }
