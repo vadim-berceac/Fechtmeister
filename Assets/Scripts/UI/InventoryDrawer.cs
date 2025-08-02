7d075a612fc6a0e534f1050ea906275526263fd9
v.berchak <v.berchak@nekki.com>
2025-08-02 16:52:53 +0300

    1

--- Assets/Scripts/UI/InventoryDrawer.cs
+++ Assets/Scripts/UI/InventoryDrawer.cs
@@ -1,9 +1,11 @@
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryDrawer : MonoBehaviour
 {
     [field: SerializeField] public InventoryUI InventoryUI { get; set; }
+   
     private void OnEnable()
     {
         UpdateInventoryCells();
@@ -18,28 +20,28 @@ public class InventoryDrawer : MonoBehaviour
        UpdateMoney();
     }
 
-    private void UpdateInventoryBag()
+    public void UpdateInventoryBag()
     {
         Debug.Log($"Показываю InventoryBag {InventoryUI.CurrentCharacter.name}");
     }
 
-    private void UpdateWeaponSystem()
+    public void UpdateWeaponSystem()
     {
         var w = InventoryUI.CurrentCharacter.Inventory.WeaponSystem.InstanceInHands.ItemData.ItemName;
         Debug.Log($"{w} WeaponSystem {InventoryUI.CurrentCharacter.name}");
     }
 
-    private void UpdateArmorSystem()
+    public void UpdateArmorSystem()
     {
         Debug.Log($"Показываю ArmorSystem {InventoryUI.CurrentCharacter.name}");
     }
 
-    private void UpdateArrows()
+    public void UpdateArrows()
     {
         Debug.Log($"Показываю Arrows {InventoryUI.CurrentCharacter.name}");
     }
 
-    private void UpdateMoney()
+    public void UpdateMoney()
     {
         Debug.Log($"Показываю Money {InventoryUI.CurrentCharacter.name}");
     }
