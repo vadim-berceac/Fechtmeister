c54e386569c8ce1bc96740864e53ac9c2467d129
v.berchak <v.berchak@nekki.com>
2025-08-13 19:49:10 +0300

    +

--- Assets/Scripts/Items/Armor/ArmorData.cs
+++ Assets/Scripts/Items/Armor/ArmorData.cs
@@ -1,6 +1,21 @@
+using System.Linq;
 using UnityEngine;
 
-public class ArmorData
+[CreateAssetMenu(fileName = "ArmorData", menuName = "Scriptable Objects/ArmorData")]
+public class ArmorData : ScriptableObject, IItemData
 {
-    
+    [field: SerializeField] public string ItemName { get; set; }
+    [field: SerializeField] public string ItemDescription { get; set; }
+    [field: SerializeField] public Sprite ItemIcon { get; set; }
+    [field: SerializeField] public GameObject EquippedModelPrefab { get; set; }
+    [field: SerializeField] public GameObject GroundedModelPrefab { get; set; }
+    [field: SerializeField] public ItemsPositions.Type ItemPosition { get; set; }
+    [field: SerializeField] public BoneData[] BoneData { get; set; }
+    [field: SerializeField] public IKBoneData IKBoneData { get; set; }
+    [field: SerializeField] public ItemDecorationData[] ItemDecorationData { get; set; }
+
+    public BoneData GetBoneData(CharacterBones.Type bone)
+    {
+        return BoneData.FirstOrDefault(b => b.BonesType == bone);
+    }
 }
