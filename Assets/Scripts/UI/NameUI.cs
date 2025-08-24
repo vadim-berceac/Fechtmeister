bd9ca552386832861d003510c7bb48fe1cd80ba8
v.berchak <v.berchak@nekki.com>
2025-08-24 10:41:16 +0300

    +

--- Assets/Scripts/UI/NameUI.cs
+++ Assets/Scripts/UI/NameUI.cs
@@ -1,11 +1,15 @@
+using System.Text.RegularExpressions;
+using Unity.Burst;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class NameUI : MonoBehaviour
 {
    [field: SerializeField] private UIDocument UIDocument { get; set; }
-  
    private IItemData _itemData;
+   private const string LabelName = "Name";
+   private const string Pattern = @"^(.+)$";
+   private const string Replacement = "[$1]";
 
    private void OnEnable()
    {
@@ -16,6 +20,8 @@ public class NameUI : MonoBehaviour
    {
       _itemData = item;
    }
+   
+   [BurstCompile]
    private void SetValues()
    {
       if (_itemData == null)
@@ -23,7 +29,8 @@ public class NameUI : MonoBehaviour
          return;
       }
       var root = UIDocument.rootVisualElement;
-      var label = root.Q<Label>("Name");
-      label.text = _itemData.ItemName;
+      var label = root.Q<Label>(LabelName);
+      var formattedText = Regex.Replace(_itemData.ItemName, Pattern, Replacement);
+      label.text = formattedText;
    }
 }
