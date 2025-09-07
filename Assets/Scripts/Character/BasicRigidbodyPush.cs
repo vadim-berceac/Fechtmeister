a5a8e1b073894667bc9d956bc1eb972592d4d20c
v.berchak <v.berchak@nekki.com>
2025-09-07 18:12:12 +0300

    +

---
+++ Assets/Scripts/Character/BasicRigidbodyPush.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class BasicRigidBodyPush : MonoBehaviour
+{
+	public LayerMask pushLayers;
+	public bool canPush;
+	[Range(0.5f, 5f)] public float strength = 1.1f;
+
+	private void OnControllerColliderHit(ControllerColliderHit hit)
+	{
+		if (canPush) PushRigidBodies(hit);
+	}
+
+	private void PushRigidBodies(ControllerColliderHit hit)
+	{
+		var body = hit.collider.attachedRigidbody;
+		if (body == null || body.isKinematic)
+		{
+			return;
+		}
+		
+		var bodyLayerMask = 1 << body.gameObject.layer;
+		if ((bodyLayerMask & pushLayers.value) == 0)
+		{
+			return;
+		}
+
+		if (hit.moveDirection.y < -0.3f)
+		{
+			return;
+		}
+
+		var pushDir = new Vector3(hit.moveDirection.x, 0.0f, hit.moveDirection.z);
+
+		body.AddForce(pushDir * strength, ForceMode.Impulse);
+	}
+}
