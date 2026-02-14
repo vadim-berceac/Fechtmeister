70b640fbdd31c39e4919864df560e78016886676
v.berchak <v.berchak@nekki.com>
2026-02-14 19:10:32 +0200

    subscene

--- Assets/Scripts/Installers/SceneContainersInstaller.cs
+++ Assets/Scripts/Installers/SceneContainersInstaller.cs
@@ -5,11 +5,13 @@ public class SceneContainersInstaller : MonoInstaller
 {
     [SerializeField] private GameObject sceneContainerPrefab;
     [SerializeField] private GameObject updateSystemPrefab;
+    [SerializeField] private GameObject sceneInterfacePrefab;
     public override void InstallBindings()
     {
         Container.Bind<SceneCharacterContainer>().FromComponentInNewPrefab(sceneContainerPrefab).AsSingle().NonLazy();
         Container.Bind<GameWindowContainer>().FromComponentInHierarchy().AsSingle().NonLazy();
         Container.Bind<CentralizedUpdateSystem>().FromComponentInNewPrefab(updateSystemPrefab).AsSingle().NonLazy();
         Container.Bind<VisionSystem>().FromComponentInHierarchy().AsSingle().NonLazy();
+        Container.Bind<SceneIntreface>().FromComponentInNewPrefab(sceneInterfacePrefab).AsSingle().NonLazy();
     }
 }
