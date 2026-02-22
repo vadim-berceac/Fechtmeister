b3fd485e319cfaca3b827eca7ef140855556d183
v.berchak <v.berchak@nekki.com>
2026-02-22 18:11:52 +0200

    bow_string_fixes

--- Assets/Scripts/Items/Weapon/Bow.cs
+++ Assets/Scripts/Items/Weapon/Bow.cs
@@ -1,66 +1,85 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 public class Bow : MonoBehaviour
 {
     [SerializeField] private LineRenderer lineRenderer;
     [SerializeField] [Range(0, 1)] private float tensionForce;
+    [SerializeField] private float tensionDelay;
+    [SerializeField] private float tensionTime;
+    [SerializeField] private Vector3 stringRestPosition = new Vector3(0, 0f, 0.15f);
     [SerializeField] private Vector3 tensionOffset = new Vector3(0, -0.1f, 0.65f);
     [SerializeField] private AudioClip tensionClip;
     [SerializeField] private AudioClip releaseClip;
-    
-    private CharacterCore _characterCore;
-    public event Action<float> OnTensionChanged;
 
-    private Vector3 _startPosition;
-    
-    private void OnValidate()
-    {
-        if (lineRenderer == null) return;
-        UpdateStringPosition(tensionForce);
-    }
+    private CharacterCore _characterCore;
+    private Coroutine _tensionCoroutine;
 
     private void Start()
     {
         _characterCore = GetComponentInParent<CharacterCore>();
-        _startPosition = lineRenderer.GetPosition(1);
-        OnTensionChanged += UpdateStringPosition;
         _characterCore.OnStateChanged += OnStateChanged;
     }
 
     private void OnDestroy()
     {
-        OnTensionChanged -= UpdateStringPosition;
         _characterCore.OnStateChanged -= OnStateChanged;
-    }
-
-    public void SetTension(float value)
-    {
-        tensionForce = Mathf.Clamp01(value);
-        OnTensionChanged?.Invoke(tensionForce);
     }
 
     private void OnStateChanged(State state)
     {
-        if (state is AimState)
+        if (state is LoadState)
         {
-            UpdateStringPosition(1);
+            if (_tensionCoroutine == null)
+                _tensionCoroutine = StartCoroutine(TensionRoutine());
+
             AudioSource.PlayClipAtPoint(tensionClip, transform.position);
-            return;
         }
-        if(tensionForce <= 0) return;
-        
-        UpdateStringPosition(0);
-        
-        if(state is not ReleaseState) return;
-        
+        if (state is LoadState || state is AimState) return;
+
+        if (_tensionCoroutine != null)
+        {
+            StopCoroutine(_tensionCoroutine);
+            _tensionCoroutine = null;
+        }
+
+        if (tensionForce <= 0) return;
+
+        UpdateStringPosition(0f);
+
+        if (state is not ReleaseState) return;
+
         AudioSource.PlayClipAtPoint(releaseClip, transform.position);
+    }
+
+    private IEnumerator TensionRoutine()
+    {
+        yield return new WaitForSeconds(tensionDelay);
+
+        if (tensionTime <= 0)
+        {
+            UpdateStringPosition(1f);
+            _tensionCoroutine = null;
+            yield break;
+        }
+
+        var elapsed = 0f;
+
+        while (elapsed < tensionTime)
+        {
+            elapsed += Time.deltaTime;
+            UpdateStringPosition(Mathf.Lerp(0f, 1f, elapsed / tensionTime));
+            yield return null;
+        }
+
+        UpdateStringPosition(1f);
+        _tensionCoroutine = null;
     }
 
     private void UpdateStringPosition(float tension)
     {
         tensionForce = tension;
-        var tensionPosition = Vector3.Lerp(_startPosition, _startPosition + tensionOffset, tension);
-        lineRenderer.SetPosition(1, tensionPosition);
+        lineRenderer.SetPosition(1, Vector3.Lerp(stringRestPosition, stringRestPosition + tensionOffset, tension));
     }
 }
