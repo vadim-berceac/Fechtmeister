4f6b69c15f9560c13cc76aa2f15d4c3b4801248e
v.berchak <v.berchak@nekki.com>
2026-02-23 20:01:17 +0200

    fix state

--- Assets/Scripts/Character/States/Humanoid/InjuredIdleState.cs
+++ Assets/Scripts/Character/States/Humanoid/InjuredIdleState.cs
@@ -11,12 +11,22 @@ public class InjuredIdleState : MovementState
             new(c => c.Health.IsHitReactionEnabled, "GetHitState"),
             new (c => Mathf.Abs(c.CharacterInputHandler.InputX) > 0 ||
                       Mathf.Abs(c.CharacterInputHandler.InputY) > 0, "WalkState"),
-            new(c => c.CharacterInputHandler.IsWeaponDraw, "WeaponOnState"),
+            new(character => (character.Inventory.IsWeaponOn), "CombatIdleState"),
             new(c => c.CharacterInputHandler.IsJump, "JumpState"),
             new(c => !c.Gravity.Grounded, "FallState"),
             new(c => c.CharacterInputHandler.IsInteract && c.TargetingSystem.HasTarget(TargetingMode.Item), "TakeLootState"),
             new(c => c.CharacterInputHandler.IsInventoryOpen, "InventoryState"),
             new(c => c.Health.CurrentHealthNormalized >= 0.5f, "IdleState"),
         };
+    }
+    
+    protected override void CheckAction(CharacterCore character)
+    {
+        base.CheckAction(character);
+        if (character.Inventory.WeaponSystem.CanDrawWeapon() 
+            && character.GraphCore.UpperBodyLayerController.IsComplete())
+        {
+            character.SetSubState(character.StatesSet.GetState("WeaponOnSubState"));
+        } 
     }
 }
